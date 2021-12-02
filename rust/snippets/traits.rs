5e5e80ee4107caeec2035e61c6d10def2e58f560
wul <li.koun@gmail.com>
2021-12-02 10:41:52 +0800

    Update traits.rs

--- rust/snippets/traits.rs
+++ rust/snippets/traits.rs
@@ -1,3 +1,6 @@
+use std::hash::{Hash, Hasher};
+
+
 // 
 // PartialEq, Self vs self
 // Self is type, self is instance
@@ -14,6 +17,31 @@ impl PartialEq for Employee {
     }
 }
 
+
+// Hash traits example
+// 如果struct内所有属性都是可hash的，可以直接用宏
+//
+#[derive(Hash)]
+struct Man {
+    name: String,
+    country: String,
+}
+
+// 或者自己实现Hash traits
+struct Person {
+    id: u32,
+    name: String,
+    phone: u64,
+}
+
+impl Hash for Person {
+    fn hash<H: Hasher>(&self, state: &mut H) {
+        self.id.hash(state);
+        self.phone.hash(state);
+    }
+}
+
+//to enable print Kind with {:?}
 #[derive(Debug)]
 enum Kind {
     A,
@@ -23,6 +51,20 @@ enum Kind {
 
 impl Default for Kind {
     fn default() -> Self { Kind::A }
+}
+
+
+
+fn about_hasher () {
+    use std::collections::hash_map::DefaultHasher;
+    use std::hash::Hasher;
+
+    let mut hasher = DefaultHasher::new();
+    let data = [0x01, 0x23, 0x45, 0x67, 0x89, 0xab, 0xcd, 0xef];
+
+    hasher.write(&data);
+    let r = hasher.finish();
+    println!("Hash is {:x}!", r);
 }
 
 #[cfg(test)]
@@ -41,10 +83,18 @@ mod tests {
         //assert_eq!(x, Kind::A);
         matches!(x, Kind::A);
     }
-  
+    #[test]
     fn test_default2() {
         let x:Kind = Kind::default();
         matches!(x, Kind::A);
     }
-  
+    #[test]
+    fn test_about_hasher() {
+        about_hasher();
+        assert!(true);
+    }
+    
 }
+
+
+
